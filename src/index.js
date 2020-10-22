ee184f3e2a8bb0424a135fe51adee717e3e6df0b
tiny-moron <68349383+tiny-moron@users.noreply.github.com>
2020-10-22 19:42:09 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split("");
+  var checkk = false;
+  if(str.length % 2 != 0) return false;
+  for(let n = 0; n < str.length; n++) {
+    if(str[n] === true) continue;
+    for(let m = 0; m < bracketsConfig.length; m++) {
+      if(str[n] === bracketsConfig[m][0]) {
+        for(let l = n + 1; l < str.length; l += 2) {
+          if(str[l] === bracketsConfig[m][1]) {
+            str[n] = true;
+            str[l] = true;
+            checkk = true;
+            break;
+          }
+        }
+      }
+    }
+    if(!(checkk)) return false;
+    checkk = false;
+  }
+  return true;
 }
